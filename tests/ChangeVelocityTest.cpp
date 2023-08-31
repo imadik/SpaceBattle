bcccbc655504292865cf50cb37f50850b7bad963
Dmitriy Ashnevits <dmitriy.ashnevits@gs-labs.ru>
2023-09-01 00:36:38 +0300

    HW4 completed

--- tests/ChangeVelocityTest.cpp
+++ tests/ChangeVelocityTest.cpp
@@ -53,48 +53,61 @@ namespace SpaceBattleTest
         EXPECT_NO_THROW(changeVelocity.execute());
     }
 
-    // TEST_F(ChangeVelocityTest, error_get_direction)
-    // {
-    //     std::any direction;
-    //     std::any angularVelocity = 33;
-    //     std::any resultDirection = SpaceBattle::Direction{60};
-    //     MockUObject mockUObject;
-    //     SpaceBattle::RotableAdapter rotableAdapter(mockUObject);
-    //     SpaceBattle::Rotate rotate(rotableAdapter);
-    //     EXPECT_CALL(mockUObject, getProperty("direction")).Times(1).WillRepeatedly(Return(direction));
-    //     EXPECT_THROW(rotate.execute(), std::bad_any_cast);
-    // }
+    TEST_F(ChangeVelocityTest, error_get_angular_velocity)
+    {
+        std::any angularVelocity;
+        std::any vectorVelocity = SpaceBattle::PositionVector{0, 14};
+        std::any resultVectorVelocity = SpaceBattle::PositionVector{10, 10};
+        MockUObject mockUObject;
+        SpaceBattle::ChangeVelocityAdapter changeVelocityAdapter(mockUObject);
+        SpaceBattle::ChangeVelocity changeVelocity(changeVelocityAdapter);
+        EXPECT_CALL(mockUObject, getProperty("angular_velocity")).Times(1).WillRepeatedly(Return(angularVelocity));
+        EXPECT_CALL(mockUObject, getProperty("velocity")).Times(1).WillRepeatedly(Return(vectorVelocity));
+        EXPECT_CALL(mockUObject, setProperty("velocity", _)).Times(0).
+            WillRepeatedly([resultVectorVelocity](std::string, std::any newVelocity)
+        {
+            EXPECT_TRUE(std::any_cast<SpaceBattle::PositionVector>(newVelocity) ==
+                std::any_cast<SpaceBattle::PositionVector>(resultVectorVelocity));
+        });
+        EXPECT_THROW(changeVelocity.execute(), std::bad_any_cast);
+    }
 
-    // TEST_F(ChangeVelocityTest, error_get_angular_velocity)
-    // {
-    //     std::any direction = SpaceBattle::Direction{27};
-    //     std::any angularVelocity;
-    //     std::any resultDirection = SpaceBattle::Direction{60};
-    //     MockUObject mockUObject;
-    //     SpaceBattle::RotableAdapter rotableAdapter(mockUObject);
-    //     SpaceBattle::Rotate rotate(rotableAdapter);
-    //     EXPECT_CALL(mockUObject, getProperty("direction")).Times(1).WillRepeatedly(Return(direction));
-    //     EXPECT_CALL(mockUObject, getProperty("angular_velocity")).Times(1).WillRepeatedly(Return(angularVelocity));
-    //     EXPECT_THROW(rotate.execute(), std::bad_any_cast);
-    // }
+    TEST_F(ChangeVelocityTest, error_get_velocity)
+    {
+        std::any angularVelocity = -45;
+        std::any vectorVelocity;
+        std::any resultVectorVelocity = SpaceBattle::PositionVector{10, 10};
+        MockUObject mockUObject;
+        SpaceBattle::ChangeVelocityAdapter changeVelocityAdapter(mockUObject);
+        SpaceBattle::ChangeVelocity changeVelocity(changeVelocityAdapter);
+        EXPECT_CALL(mockUObject, getProperty("angular_velocity")).Times(0).WillRepeatedly(Return(angularVelocity));
+        EXPECT_CALL(mockUObject, getProperty("velocity")).Times(1).WillRepeatedly(Return(vectorVelocity));
+        EXPECT_CALL(mockUObject, setProperty("velocity", _)).Times(0).
+            WillRepeatedly([resultVectorVelocity](std::string, std::any newVelocity)
+        {
+            EXPECT_TRUE(std::any_cast<SpaceBattle::PositionVector>(newVelocity) ==
+                std::any_cast<SpaceBattle::PositionVector>(resultVectorVelocity));
+        });
+        EXPECT_THROW(changeVelocity.execute(), std::bad_any_cast);
+    }
 
-    // TEST_F(ChangeVelocityTest, error_set_direction)
-    // {
-    //     std::any direction = SpaceBattle::Direction{27};
-    //     std::any angularVelocity = 33;
-    //     std::any resultDirection = SpaceBattle::Direction{60};
-    //     MockUObject mockUObject;
-    //     SpaceBattle::RotableAdapter rotableAdapter(mockUObject);
-    //     SpaceBattle::Rotate rotate(rotableAdapter);
-    //     EXPECT_CALL(mockUObject, getProperty("direction")).Times(1).WillRepeatedly(Return(direction));
-    //     EXPECT_CALL(mockUObject, getProperty("angular_velocity")).Times(1).WillRepeatedly(Return(angularVelocity));
-    //     EXPECT_CALL(mockUObject, setProperty("direction", _)).Times(1).
-    //         WillRepeatedly([resultDirection](std::string, std::any newDirection)
-    //     {
-    //         EXPECT_TRUE(std::any_cast<SpaceBattle::Direction>(newDirection) ==
-    //             std::any_cast<SpaceBattle::Direction>(resultDirection));
-    //         throw std::invalid_argument("");
-    //     });
-    //     EXPECT_THROW(rotate.execute(), std::invalid_argument);
-    // }
+    TEST_F(ChangeVelocityTest, error_set_velocity)
+    {
+        std::any angularVelocity = -45;
+        std::any vectorVelocity = SpaceBattle::PositionVector{0, 14};
+        std::any resultVectorVelocity = SpaceBattle::PositionVector{10, 10};
+        MockUObject mockUObject;
+        SpaceBattle::ChangeVelocityAdapter changeVelocityAdapter(mockUObject);
+        SpaceBattle::ChangeVelocity changeVelocity(changeVelocityAdapter);
+        EXPECT_CALL(mockUObject, getProperty("angular_velocity")).Times(1).WillRepeatedly(Return(angularVelocity));
+        EXPECT_CALL(mockUObject, getProperty("velocity")).Times(1).WillRepeatedly(Return(vectorVelocity));
+        EXPECT_CALL(mockUObject, setProperty("velocity", _)).Times(1).
+            WillRepeatedly([resultVectorVelocity](std::string, std::any newVelocity)
+        {
+            EXPECT_TRUE(std::any_cast<SpaceBattle::PositionVector>(newVelocity) ==
+                std::any_cast<SpaceBattle::PositionVector>(resultVectorVelocity));
+            throw std::invalid_argument("");
+        });
+        EXPECT_THROW(changeVelocity.execute(), std::invalid_argument);
+    }
 }
--- tests/MacroCommandTest.cpp
+++ tests/MacroCommandTest.cpp
@@ -3,6 +3,10 @@
 #include "Adapters/FuelAdapters/CheckFuelAdapter.h"
 #include "Commands/BurnFuel/BurnFuel.h"
 #include "Adapters/FuelAdapters/BurnFuelAdapter.h"
+#include "Commands/ChangeVelocity/ChangeVelocity.h"
+#include "Adapters/MoveAdapters/ChangeVelocityAdapter.h"
+#include "Commands/Rotate/Rotate.h"
+#include "Adapters/MoveAdapters/RotableAdapter.h"
 #include "Exceptions/CommandException.h"
 #include "MockObjects/MockObjects.h"
 
@@ -58,39 +62,35 @@ namespace SpaceBattleTest
         EXPECT_THROW(macroCommand.execute(), SpaceBattle::CommandException);
     }
 
-    // TEST_F(MacroCommandTest, not_enought_fuel)
-    // {
-    //     std::any availableFuel = unsigned(5);
-    //     std::any fuelConsumption = unsigned(10);
-    //     MockUObject mockUObject;
-    //     SpaceBattle::CheckFuelAdapter checkFuelAdapter(mockUObject);
-    //     SpaceBattle::CheckFuel checkFuel(checkFuelAdapter);
-    //     EXPECT_CALL(mockUObject, getProperty("available_fuel")).Times(1).WillRepeatedly(Return(availableFuel));
-    //     EXPECT_CALL(mockUObject, getProperty("fuel_consumption")).Times(1).WillRepeatedly(Return(fuelConsumption));
-    //     EXPECT_THROW(checkFuel.execute(), SpaceBattle::FuelException);
-    // }
-
-    // TEST_F(MacroCommandTest, error_get_available_fuel)
-    // {
-    //     std::any availableFuel;
-    //     std::any fuelConsumption = unsigned(5);
-    //     MockUObject mockUObject;
-    //     SpaceBattle::CheckFuelAdapter checkFuelAdapter(mockUObject);
-    //     SpaceBattle::CheckFuel checkFuel(checkFuelAdapter);
-    //     EXPECT_CALL(mockUObject, getProperty("available_fuel")).Times(1).WillRepeatedly(Return(availableFuel));
-    //     EXPECT_CALL(mockUObject, getProperty("fuel_consumption")).Times(0);
-    //     EXPECT_THROW(checkFuel.execute(), std::bad_any_cast);
-    // }
-
-    // TEST_F(MacroCommandTest, error_get_fuel_consumption)
-    // {
-    //     std::any availableFuel = unsigned(10);
-    //     std::any fuelConsumption;
-    //     MockUObject mockUObject;
-    //     SpaceBattle::CheckFuelAdapter checkFuelAdapter(mockUObject);
-    //     SpaceBattle::CheckFuel checkFuel(checkFuelAdapter);
-    //     EXPECT_CALL(mockUObject, getProperty("available_fuel")).Times(1).WillRepeatedly(Return(availableFuel));
-    //     EXPECT_CALL(mockUObject, getProperty("fuel_consumption")).Times(1).WillRepeatedly(Return(fuelConsumption));
-    //     EXPECT_THROW(checkFuel.execute(), std::bad_any_cast);
-    // }
+    TEST_F(MacroCommandTest, rotate_and_change_velocity)
+    {
+        std::vector<std::unique_ptr<SpaceBattle::ICommand>> commands;
+        std::any angularVelocity = 45;
+        std::any vectorVelocity = SpaceBattle::PositionVector{10, 10};
+        std::any resultVectorVelocity = SpaceBattle::PositionVector{0, 14};
+        MockUObject mockUObject;
+        SpaceBattle::ChangeVelocityAdapter changeVelocityAdapter(mockUObject);
+        std::any direction = SpaceBattle::Direction{15};
+        std::any resultDirection = SpaceBattle::Direction{60};
+        SpaceBattle::RotableAdapter rotableAdapter(mockUObject);
+        commands.push_back(std::make_unique<SpaceBattle::ChangeVelocity>(changeVelocityAdapter));
+        commands.push_back(std::make_unique<SpaceBattle::Rotate>(rotableAdapter));
+        SpaceBattle::MacroCommand macroCommand(std::move(commands));
+        EXPECT_CALL(mockUObject, getProperty("angular_velocity")).Times(2).WillRepeatedly(Return(angularVelocity));
+        EXPECT_CALL(mockUObject, getProperty("velocity")).Times(1).WillRepeatedly(Return(vectorVelocity));
+        EXPECT_CALL(mockUObject, setProperty("velocity", _)).Times(1).
+            WillRepeatedly([resultVectorVelocity](std::string, std::any newVelocity)
+        {
+            EXPECT_TRUE(std::any_cast<SpaceBattle::PositionVector>(newVelocity) ==
+                std::any_cast<SpaceBattle::PositionVector>(resultVectorVelocity));
+        });
+        EXPECT_CALL(mockUObject, getProperty("direction")).Times(1).WillRepeatedly(Return(direction));
+        EXPECT_CALL(mockUObject, setProperty("direction", _)).Times(1).
+            WillRepeatedly([resultDirection](std::string, std::any newDirection)
+        {
+            EXPECT_TRUE(std::any_cast<SpaceBattle::Direction>(newDirection) ==
+                std::any_cast<SpaceBattle::Direction>(resultDirection));
+        });
+        EXPECT_NO_THROW(macroCommand.execute());
+    }
 }
